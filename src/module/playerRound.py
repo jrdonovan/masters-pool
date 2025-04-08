32cdc3ba565b5c35aaff019d021918c705bffa05
Joe Donovan <jrdonovan1004@gmail.com>
2025-04-08 14:14:25 -0600

    Update PlayerRound class

--- src/module/playerRound.py
+++ src/module/playerRound.py
@@ -1,44 +1,61 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
+
+from src.module.hole import Hole
 
 @dataclass
 class PlayerRound:
     """
     Represents a player's round in a golf tournament.
     Attributes:
+        _round_id (int): The ID of the round.
         _player_id (int): The ID of the player.
-        _thru (str): The current hole the player is on.
-        _tee_time (str): The tee time of the player.
+        _complete (bool): Indicates if the round is complete.
+        _starting_hole (int): The hole where the player started.
+        _current_hole (int): The current hole the player is on.
+        _current_round_score (str): The current score of the player in the round.
+        _strokes (int): The number of strokes taken by the player in the round.
+        _holes (List[Hole]): A list of Hole objects representing the holes played in the round.
     """
+    _round_id: int
     _player_id: int
-    _thru: str
-    _tee_time: str
+    _complete: bool
+    _starting_hole: int
+    _current_hole: int
+    _current_round_score: str
+    _strokes: int
+    _holes: List[Hole] = field(init=False)
 
     @property
     def player_id(self):
         return self._player_id
 
-    @player_id.setter
-    def player_id(self, value: int):
-        if not isinstance(value, int):
-            raise ValueError("Player ID must be an integer.")
-        self._player_id = value
+    @property
+    def complete(self):
+        return self._complete
 
     @property
-    def thru(self):
-        return self._thru
-
-    @thru.setter
-    def thru(self, value: str):
-        if not isinstance(value, str):
-            raise ValueError("Thru must be a string.")
-        self._thru = value
+    def starting_hole(self):
+        return self._starting_hole
 
     @property
-    def tee_time(self):
-        return self._tee_time
+    def current_hole(self):
+        return self._current_hole
 
-    @tee_time.setter
-    def tee_time(self, value: str):
-        if not isinstance(value, str):
-            raise ValueError("Tee time must be a string.")
-        self._tee_time = value
+    @property
+    def current_round_score(self):
+        return self._current_round_score
+
+    @property
+    def strokes(self):
+        return self._strokes
+
+    @property
+    def holes(self):
+        return self._holes
+
+    @holes.setter
+    def holes(self, value: List[Hole]):
+        if not isinstance(value, list):
+            raise ValueError("Holes must be a list.")
+        self._holes = value
