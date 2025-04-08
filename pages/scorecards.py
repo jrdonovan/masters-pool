1e0fdfcc4d1da13be421867b06f429435ab87b31
Joe Donovan <jrdonovan1004@gmail.com>
2025-04-08 10:57:38 -0600

    Scorecards page

---
+++ pages/scorecards.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Scorecards", page_icon=":golfer:", layout="wide")
+st.title("Scorecards")
+st.write(
+    "Scorecards for each golfer in the tournament."
+)
