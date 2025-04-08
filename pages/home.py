d27e49ff4a2dccd56adcc18dc8a651c62ef3c57c
Joe Donovan <jrdonovan1004@gmail.com>
2025-04-08 10:57:18 -0600

    Home page

---
+++ pages/home.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.set_page_config(page_title="Masters Pool Leaderboard", layout="wide")
+st.title("2025 Masters Pool")
+
+st.markdown(
+    """
+    Participants select 6 players.
+
+    Players are divided into 4 tiers:
+    1. BSDs (2)
+    2. Tuna Fishies (2)
+    3. Sleepers (1)
+    4. Happy Bois (1)
+
+    A required tiebreaker question is included at the bottom.
+
+    ## Fees
+    - $25 per entry. Max of 2 entries per person.
+    - Venmo your entry fee to [@Mark-Chanatry](https://venmo.com/u/Mark-Chanatry). Money must be submitted before 6 AM ET on Thursday. Entries will be voided if money is not received by then.
+    - To simplify the process of determining who's paid, please include the name of your entry in the Venmo description _(Ex. Masters Pool - Joe Donovan 1)_
+    - If Venmo is not a viable option for you for some reason, reach out to Mark at **315-941-8903** and he'll figure it out for you.
+
+    ## Payouts
+    1. 70% of the total pot
+    2. 20% of the total pot
+    3. 10% of the total pot
+
+    ## Scoring
+    Please refer to the [FanDuel scoring rules](https://www.fanduel.com/fantasy-golf?t=rules#scoring) for information on how the scoring works.
+    """
+)
