ed6b5f81a316824035440a4af237e39d4a094924
Joe Donovan <jrdonovan1004@gmail.com>
2025-04-08 10:57:29 -0600

    Pool leaderboard page

---
+++ pages/pool_leaderboard.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Pool Leaderboard", page_icon=":golfer:", layout="wide")
+st.title("Pool Leaderboard")
+st.write(
+    "The leaderboard is a summary of the entries and their scores. It is updated every 5 minutes."
+)
