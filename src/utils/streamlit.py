6aa2a0b43151094c7ab6fc87ad90c07f3a738a0b
Joe Donovan <jrdonovan1004@gmail.com>
2025-04-09 22:36:16 -0600

    Streamlit utilities functions

--- src/utils/streamlit.py
+++ src/utils/streamlit.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from typing import Any
+
 
 def get_secret(key: str):
     """
@@ -8,3 +10,30 @@ def get_secret(key: str):
         return st.secrets[key]
     else:
         raise KeyError(f"Secret '{key}' not found in Streamlit secrets.")
+
+
+def session_key_exists(key: str) -> bool:
+    """
+    Checks if a key exists in the Streamlit session state.
+    """
+    return (key in st.session_state) and (st.session_state[key] is not None)
+
+
+def set_session_state(key: str, value: Any) -> None:
+    """
+    Sets a value in the Streamlit session state.
+    """
+    st.session_state[key] = value
+
+
+def get_session_state(key: str = None):
+    """
+    Fetches a value from the Streamlit session state.
+    """
+    if key:
+        if key in st.session_state:
+            return st.session_state[key]
+        else:
+            raise KeyError(f"Key '{key}' not found in Streamlit session state.")
+    else:
+        return st.session_state
